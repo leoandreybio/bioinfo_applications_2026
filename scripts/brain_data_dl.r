5a016cf4dfba8acca610a309ff24403ad8bd4d64
Leo Andrey <leoandreybio@gmail.com>
2026-03-16 15:15:24 +0100

    final commit

--- scripts/brain_data_dl.r
+++ scripts/brain_data_dl.r
@@ -7,9 +7,6 @@
 
 # libraries --------------------------------------------------------------------
 
-#if (!dir.exists("~/R/x86_64-pc-linux-gnu-library/")) {
-#  dir.create("~/R/x86_64-pc-linux-gnu-library/", recursive = TRUE)
-#}
 #remotes::install_github("jonchang/fishtree", lib = "~/R/x86_64-pc-linux-gnu-library/")
 #remotes::install_github("ropensci/rfishbase", lib = "~/R/x86_64-pc-linux-gnu-library/")
 
@@ -20,12 +17,14 @@ library(ape) # manipulation of phylogenetic trees
 library(phytools) # manipulation and visualisation of phylogenetic data
 library(geiger) # tools for evolution models and manipulation of trees
 library(tidyverse) # dplyr and ggplot among others
-library(Cairo)
 
+# 1 Traits database ------------------------------------------------------------
 
-######### télécharger les traits de la database
+# traits db download
 raw_brains <- rfishbase::fb_tbl("brains")
 glimpse(raw_brains)
+
+# removing of NAs and calculation of brain residuals
 brains <- raw_brains %>%
   filter(!is.na(BodyWeight), !is.na(BrainWeight), !is.na(SpecCode)) %>%
   group_by(SpecCode) %>%
@@ -36,38 +35,29 @@ brains <- raw_brains %>%
   mutate(
     rel_brain_size = residuals(lm(log10(brain_weight) ~ log10(body_weight)))
   )
+# residuals visualisation
 plot(log10(brains$brain_weight) ~ log10(brains$body_weight))
 hist(brains$rel_brain_size)
 
+# add species names to the data
 spec_names <- rfishbase::species_names()
-
 brains <- inner_join(spec_names, brains, join_by(SpecCode))
 glimpse(brains)
 
-####### étlécharger la phylogénie
+# 2 Phylogenetic tree ------------------------------------------------------------
 
-# phylo object type :
-# str(phy)
-# phy$edge => tree structure
-# phy$edge.length => length of the branches specified in phy$edge
-# phy$tip.label => OTU (species) names
-# plot(phy, "fan")
-# plot(phy, "phylogram")
-# plot(phy, "cladogram")
-# phy2 <- drop.tip(phy, c(species_names)) => removes species
-# phy3 <- keep.tip(phy, c(species_names)) => removes all but some species
-
+# downloading a fish phylogeny
 phy <- fishtree::fishtree_phylogeny()
-phy
-windows()
-par(mfrow = c(2, 1))
-plot(phy, show.tip.label = FALSE)
-ape::ltt.plot(phy)
+str(phy)
 
-###### les noms des espèces et pruning de la phylogénie
+# standardising species names
 phy$tip.label <- gsub("_", " ", phy$tip.label)
 phy$tip.label
 
+# 3 Matching the db and the tree -------------------------------------------------
+
+# matching name convention between traits db and tree
+# method 1 with WORMS
 checknames <- function(speclist) {
   correct_names_list <- c()
 
@@ -88,25 +78,33 @@ checknames <- function(speclist) {
 worms_check_phy <- checknames(phy$tip.label) # does not contain freshwater species
 worms_check_fb <- checknames(spec_names)
 
+# method 2 with FishBase
 fishbase_check_phy <- lapply(phy$tip.label, rfishbase::validate_names) # takes too much time
 
-
+# pruning the tree to the database species
+# checking which species names are too much in the tree
 checked_names <- geiger::name.check(phy, data.names = brains$Species)
 length(brains$Species)
 length(checked_names$data_not_tree)
 
+# removing those tips and associated nodes
 length(phy$tip.label)
 brainphy <- ape::drop.tip(phy, checked_names$tree_not_data)
 length(brainphy$tip.label)
 
+# ordering the db to the order of the tree
 brains <- filter(brains, Species %in% brainphy$tip.label)
 brains <- brains[match(brainphy$tip.label, brains$Species), ]
 length(brains$Species)
 
+# 4 Visualise the trait onto the phylogeny ----------------------------------------
+
+# create a vector with the brain size ans species names
 brainvector <- brains$rel_brain_size
 names(brainvector) <- brains$Species
 
-pdf(file = "contMap_plot.pdf", width = 35, height = 35) # Set dimensions
+# save the contmap graph in a PDF
+pdf(file = "contMap_plot.pdf", width = 35, height = 35) # set dimensions
 contMap(
   brainphy,
   brainvector,
@@ -114,4 +112,4 @@ contMap(
   fsize = c(0.7, 1.1),
   outline = FALSE
 )
-dev.off() # Close the device
+dev.off()
--- tutorials/tutorial_phylo_trait_analysis.md
+++ tutorials/tutorial_phylo_trait_analysis.md
@@ -10,16 +10,70 @@ A bonus step will be to make visualisation of the mapped traits on the tree.
 
 ## Steps
 
-### 1 Phylogenetic tree
+### 1 Traits database
 
-- Newick format
+- Find a database containing the traits you are interested in.  
+Download the table, directly on their web-portal, using their API or a dedicated R package.  
+Depending on the format that you get, you might want to prune the dataset to only the variables you are interested in and put all relevant data into a single dataframe/tibble.
 
-- NEXUS format
+> Don't forget to download a table containing the traits but also a way to **identify the species** (e.g. scientific names, database codes).
 
-### 2
+- Check for missing data and remove all empty lines.
 
-### 3
+### 2 Phylogenetic trees
 
-### 4
+In R, phylogenetic trees come in objects called *phylo*. This type of object is a list containing:
+1. `phylo$edge` contains a matrix of 2 columns containing numbers. Numbers represent a tip a node of the tree. On each row of this matrix, the left number is the parent node and the right number is the child node/tip. All connections (= branches) of the tree are specified in there.
+2. `phylo$edge.length` contains a vector of the same length that the edge matrix. Each value is the length of the corresponding branch in the edge matrix.
+3. `phylo$tip.label` contains a vector with the names of the tips (species names).
 
-## Conclusion
+- Download a tree either in a database or using a R package. You might get different formats depending on the source, you can then transform it into the *phylo* format using an appropriate function. For example, *hclust* objects can be transformed into *phylo* objects with the `as.phylo` command from package `ape`.
+
+- Species names in trees are generally written *Genus_species*, while there are generally written *Genus species* in databases. You can quickly modify the tree species list.
+``` 
+phy$tip.label <- gsub("_", " ", phy$tip.label)
+```
+
+### 3 Matching the db and the tree
+
+For the analysis to work, you need to use matching species (or other OTU) names in the database and the tree, as it will be your link between the phylogenetic data and the traits data. Several databases provide detailed taxonomic information allowing you to check synonyms, old names, etc.
+
+> I propose two alternatives in my example, but one is failing because the WORMS database contains data for marine species only, and the methode using the fishbase package is very time-consuming.
+
+- Pass both you db species list and you tip labels through a taxonomic database to get matching names and remove synonyms and old names.
+
+- Prune the tree to species that match your tip labels and dp species.
+```
+# checks tip labels against a vector of species names
+checked_names <- geiger::name.check(phy, data.names = db$species)
+
+# uses the information of checked_names to prune the tree
+newphy <- ape::drop.tip(phy, checked_names$tree_not_data)
+```
+
+- Reorder the db to the same order of the tree so that species names and tip labels match.
+```
+db <- filter(db, pecies %in% newphy$tip.label)
+db <- db[match(newphy$tip.label, db$species), ]
+```
+
+### 4 Visualise the trait onto the phylogeny
+
+- Extract the variable (trait) that you are interested in into a vector and put the species names as the names of this vector.
+```
+traitvector <- db$trait
+names(traitvector) <- db$species
+```
+
+- Build a coloured phylogenetic tree where colour is dependent on the value of the (continuous) trait. Save it in a PDF (vectorised format).
+```
+pdf(file = "contMap_plot.pdf", width = 35, height = 35) # set dimensions
+contMap(
+  brainphy,
+  brainvector,
+  type = "fan",
+  fsize = c(0.7, 1.1),
+  outline = FALSE
+)
+dev.off() 
+```
