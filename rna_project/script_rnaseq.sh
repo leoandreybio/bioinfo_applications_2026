266717901aab406c69c9e1e8804a5ccb755d84a7
Léo Andrey <leoandreybio@gmail.com>
2026-05-04 11:54:10 +0200

    downloading the reference genome

--- rna_project/script_rnaseq.sh
+++ rna_project/script_rnaseq.sh
@@ -9,27 +9,36 @@ fastq-dump --split-files -O data/rnaseq SRR17844033
 
 # Running FastQC for quality control
 
-# Loading the module
+## Loading the module
 module load FastQC
 
-# Repository to store the reports
+## Repository to store the reports
 mkdir -p reports/fastqc_report
 
-# Running fastqc on both files
+## Running fastqc on both files
 fastqc -o reports/fastqc_report data/rnaseq/SRR17844033_1.fastq
 fastqc -o reports/fastqc_report data/rnaseq/SRR17844033_2.fastq
 
 # Trimming the reads using Trimmomatic
 
-# Loading the module
+## Loading the module
 module load Trimmomatic
 
-# Repository to store the trimmed reads
+## Repository to store the trimmed reads
 mkdir -p data/rnaseq/trimmed
 
-# Running Trimmomatic on both files
+## Running Trimmomatic on both files
 trimmomatic PE -threads 4 -phred33 \
 data/rnaseq/SRR17844033_1.fastq data/rnaseq/SRR17844033_2.fastq \
 data/rnaseq/trimmed/SRR17844033_1.paired.fastq data/rnaseq/trimmed/SRR17844033_1.unpaired.fastq \
 data/rnaseq/trimmed/SRR17844033_2.paired.fastq data/rnaseq/trimmed/SRR17844033_2.unpaired.fastq \
 ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+# Aligning the reads to the reference genome using HISAT2
+
+## Downloading the reference genome
+mkdir -p genomes/conger_conger
+
+## Download all files from the NCBI genome directory
+cd genomes/conger_conger
+wget -r -np -nH --cut-dirs=7 ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/963/514/075/
