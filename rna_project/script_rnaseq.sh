4fcb93e89d014382b1d94b35ceebba9854e64ffb
Léo Andrey <leoandreybio@gmail.com>
2026-05-04 09:28:32 +0200

    first fastqc report

--- rna_project/script_rnaseq.sh
+++ rna_project/script_rnaseq.sh
@@ -7,4 +7,14 @@ mkdir -p data/rnaseq
 module load SRA-Toolkit
 fastq-dump --split-files -O data/rnaseq SRR17844033
 
+# Running FastQC for quality control
 
+# Loading the module
+module load FastQC
+
+# Repository to store the reports
+mkdir -p reports/fastqc_report
+
+# Running fastqc on both files
+fastqc -o reports/fastqc_report data/rnaseq/SRR17844033_1.fastq
+fastqc -o reports/fastqc_report data/rnaseq/SRR17844033_2.fastq
